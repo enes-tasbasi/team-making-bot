307cc535d993fdfe2032eb1885d4104559f929f7
= <=>
2022-11-20 15:24:21 -0500

    Get team assignments working

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMembers,
     // GatewayIntentBits.GuildMembers,
     // GatewayIntentBits.DirectMessages,
   ],
@@ -38,131 +40,62 @@ client.on("messageCreate", (message) => {
           `Please provide a number, ${message.author}!`
         );
       }
-      const channels = message.guild.channels.cache.filter(
-        (c) => c.type === "voice"
-      );
-      // let users = [
-      //   "anishkasi",
-      //   "Wolfinthehouse",
-      //   "pindabc",
-      //   "aprbhd",
-      //   "JakeSuli",
-      //   "gopuman",
-      //   "akshara",
-      //   "greybeard278",
-      //   "Dobby",
-      // ];
-      var users = [];
+      // const channels = message.guild.channels.cache.filter(
+      //   (c) => c.type === "voice"
+      // );
 
-      for (const [channelID, channel] of channels) {
-        for (const [memberID, member] of channel.members) {
-          users.push(member.user.username);
-        }
-      }
+      client.guilds.fetch(message.guildId).then((guild) => {
+        guild.members.fetch({ withPresences: true }).then((fetchedMembers) => {
+          console.log(fetchedMembers);
 
-      let mentions = args.slice(1);
-      var ids = [];
+          const users = [];
 
-      if (mentions.length) {
-        var exclude = [];
-        mentions.forEach((ex) => {
-          var id = getUserFromMention(ex);
-          ids.push(id);
-          var userObj = client.users.cache.get(id);
-          if (userObj) exclude.push(userObj.username);
+          fetchedMembers.forEach((user) => users.push(user));
+
+          console.log(users);
+
+          const ids = [];
+          ids.unshift(args[0]);
+          var params = ids.join("-");
+
+          let numTeams = parseInt(args[0]);
+          let teamSize = Math.round(users.length / numTeams);
+          if (teamSize <= 1)
+            return message.channel.send(
+              `Not enough people for this team size, ${message.author}!`
+            );
+          var teams = makeTeams(users, numTeams); // works
+
+          const text = teams
+            .map((team, index) => {
+              const teamStr = team.map((user) => user.displayName).join(", ");
+
+              return `Team ${index + 1}: ${teamStr}`;
+            })
+            .join("\n");
+
+          // var msg = createMessage(teams, params);
+          message.channel.send(text);
         });
-        users = users.filter((i) => exclude.indexOf(i) === -1);
-      }
+      });
 
-      ids.unshift(args[0]);
-      var params = ids.join("-");
+      // mentions has an array of arguments that correspond to the user id
+      // !teams 2 @Enes
+      // mentions will have the userId of @Enes in the above case
+      // let mentions = args.slice(1);
 
-      let numTeams = parseInt(args[0]);
-      let teamSize = Math.round(users.length / numTeams);
-      if (teamSize <= 1)
-        return message.channel.send(
-          `Not enough people for this team size, ${message.author}!`
-        );
-      var teams = makeTeams(users, numTeams);
+      // if (mentions.length) {
+      //   var exclude = [];
+      //   mentions.forEach((ex) => {
+      //     var id = getUserFromMention(ex);
+      //     ids.push(id);
+      //     var userObj = client.users.cache.get(id);
+      //     if (userObj) exclude.push(userObj.username);
+      //   });
+      //   users = users.filter((i) => exclude.indexOf(i) === -1);
+      // }
 
-      var msg = createMessage(teams, params);
-      message.channel.send({ embed: msg });
-    }
-  }
-});
-
-client.on("message", (message) => {
-  console.log(message.content);
-  if (!message.author.bot) {
-    message.channel.send(`Message sent`);
-  }
-
-  if (
-    message.content.startsWith(prefix) &&
-    (message.author.id == client.user.id || !message.author.bot)
-  ) {
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === "teams") {
-      if (!args.length) {
-        return message.channel.send(
-          `You didn't provide any arguments, ${message.author}!`
-        );
-      } else if (!parseInt(args[0])) {
-        return message.channel.send(
-          `Please provide a number, ${message.author}!`
-        );
-      }
-      const channels = message.guild.channels.cache.filter(
-        (c) => c.type === "voice"
-      );
-      // let users = [
-      //   "anishkasi",
-      //   "Wolfinthehouse",
-      //   "pindabc",
-      //   "aprbhd",
-      //   "JakeSuli",
-      //   "gopuman",
-      //   "akshara",
-      //   "greybeard278",
-      //   "Dobby",
-      // ];
-      var users = [];
-
-      for (const [channelID, channel] of channels) {
-        for (const [memberID, member] of channel.members) {
-          users.push(member.user.username);
-        }
-      }
-
-      let mentions = args.slice(1);
-      var ids = [];
-
-      if (mentions.length) {
-        var exclude = [];
-        mentions.forEach((ex) => {
-          var id = getUserFromMention(ex);
-          ids.push(id);
-          var userObj = client.users.cache.get(id);
-          if (userObj) exclude.push(userObj.username);
-        });
-        users = users.filter((i) => exclude.indexOf(i) === -1);
-      }
-
-      ids.unshift(args[0]);
-      var params = ids.join("-");
-
-      let numTeams = parseInt(args[0]);
-      let teamSize = Math.round(users.length / numTeams);
-      if (teamSize <= 1)
-        return message.channel.send(
-          `Not enough people for this team size, ${message.author}!`
-        );
-      var teams = makeTeams(users, numTeams);
-
-      var msg = createMessage(teams, params);
-      message.channel.send({ embed: msg });
+      // var ids = [];
     }
   }
 });
