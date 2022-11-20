0467eeffc9f9b81c9d0b97627560567cf2647b74
= <=>
2022-11-20 14:16:06 -0500

    Set correct permissions and update on command

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const keep_alive = require("./keep_alive.js");
-const Discord = require("discord.js");
-const client = new Discord.Client({
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
   intents: [
-    // GatewayIntentBits.Guilds,
-    // GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
     // GatewayIntentBits.GuildMembers,
     // GatewayIntentBits.DirectMessages,
   ],
@@ -17,6 +18,77 @@ const prefix = "!";
 
 client.on("ready", () => {
   console.log(client.user.username, "is running");
+});
+
+client.on("messageCreate", (message) => {
+  if (
+    message.content.startsWith(prefix) &&
+    /* message.author.id == client.user.id || */ !message.author.bot
+  ) {
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === "teams") {
+      if (!args.length) {
+        return message.channel.send(
+          `You didn't provide any arguments, ${message.author}!`
+        );
+      } else if (!parseInt(args[0])) {
+        return message.channel.send(
+          `Please provide a number, ${message.author}!`
+        );
+      }
+      const channels = message.guild.channels.cache.filter(
+        (c) => c.type === "voice"
+      );
+      // let users = [
+      //   "anishkasi",
+      //   "Wolfinthehouse",
+      //   "pindabc",
+      //   "aprbhd",
+      //   "JakeSuli",
+      //   "gopuman",
+      //   "akshara",
+      //   "greybeard278",
+      //   "Dobby",
+      // ];
+      var users = [];
+
+      for (const [channelID, channel] of channels) {
+        for (const [memberID, member] of channel.members) {
+          users.push(member.user.username);
+        }
+      }
+
+      let mentions = args.slice(1);
+      var ids = [];
+
+      if (mentions.length) {
+        var exclude = [];
+        mentions.forEach((ex) => {
+          var id = getUserFromMention(ex);
+          ids.push(id);
+          var userObj = client.users.cache.get(id);
+          if (userObj) exclude.push(userObj.username);
+        });
+        users = users.filter((i) => exclude.indexOf(i) === -1);
+      }
+
+      ids.unshift(args[0]);
+      var params = ids.join("-");
+
+      let numTeams = parseInt(args[0]);
+      let teamSize = Math.round(users.length / numTeams);
+      if (teamSize <= 1)
+        return message.channel.send(
+          `Not enough people for this team size, ${message.author}!`
+        );
+      var teams = makeTeams(users, numTeams);
+
+      var msg = createMessage(teams, params);
+      message.channel.send({ embed: msg });
+    }
+  }
 });
 
 client.on("message", (message) => {
